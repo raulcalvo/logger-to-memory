404d556661b6f5e1e8a69c4473d505c7bfd78594
Raul Calvo <raulcalvo@gmail.com>
2020-01-11 12:23:27 +0100

    Evolution

--- index.js
+++ index.js
@@ -1,36 +1,51 @@
 'use strict';
-const fs = require('fs');
-const mergeJSON = require('merge-json');
+const requirer = require("../extended-requirer/index.js");
+const r = new requirer(__dirname);
+
+const path = r.require('path');
+const fs = r.require('fs');
+const mergeJSON = r.require('merge-json');
+const configLoader = r.require('config-loader-manager');
+
+function getModuleName(){
+    return __dirname.split(path.sep).slice(-1)[0];
+}
 
 module.exports = class logger {
     constructor(config) {
-        var defaultConfig = {
-            "logsEnabled" : true,   // If false logs won't do anything
-            "maxLogLines" : 20,     // Max number of lines/entrys in memory log
-            "logToConsole" : true,  // Log also to console.log("Some log")
-            "lineSeparator" : "<br>"// Line separator when output logs with get function
+        var defaultConfig = {};
+        defaultConfig[getModuleName()] = {
+            "logsEnabled" : true,       // If false logs won't do anything
+            "maxLogLines" : 20,         // Max number of lines/entrys in memory log
+            "logToConsole" : true,      // Log also to console.log("Some log")
+            "lineSeparator" : "<br>",   // Line separator when output logs with get function
+            "firstLogLine" : "raulcalvo/logger-to-memory logs:"
         };
-        this._c = mergeJSON.merge(defaultConfig, config);
-        this._log = ["raulcalvo/ocr online _log system" + this._c.lineSeparator];
+        this._config = configLoader.load(__dirname, config, defaultConfig);
+
+        this._log = [this.getConfig("firstLogLine") + this.getConfig("lineSeparator")];
         this._logNumber = 0;
     }
 
+    getConfig(key){
+        return this._config[__dirname.split(path.sep).slice(-1)[0]][key];
+    }
     log(out) {
-        if (!this._c.logsEnabled)
+        if (!this.getConfig("logsEnabled"))
             return;
-        if (this._log.length > this._c.maxLogLines)
+        if (this._log.length > this.getConfig("maxLogLines"))
             this._log.splice(1, 1);
         const dateTime = new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '');
         var log = "[" + ++this._logNumber + "][" + dateTime + "] " + out;
         this._log.push(log);
-        if ( this._c.logToConsole)
+        if ( this.getConfig("logToConsole"))
             console.log(log);
     }
 
     get() {
         var out = "";
         this._log.forEach(value => {
-            out += value + this._c.lineSeparator;
+            out += value + this.getConfig("lineSeparator");
         });
         return out;
     }
